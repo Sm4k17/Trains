4fc36e8339a45c0bebafd7bb47f45cda6d8cca30
Rustam Khanakhmedov <bateg@mail.ru>
2026-01-12 20:05:33 +0300

    Переход на MVVM: создан MainTabViewModel, вынесена вся бизнес-логика из View.

---
+++ Trains/Views/Main/MainTabViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabViewModel.swift
+//  Trains
+//
+//  Created by Рустам Ханахмедов on 12.01.2026.
+//
+
+import SwiftUI
+
+struct MainTabViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabViewModel()
+}
