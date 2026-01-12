5d81c8cd647eac916c3c652c903a48584648a521
Rustam Khanakhmedov <bateg@mail.ru>
2026-01-12 20:41:42 +0300

    Переход на MVVM: создан ScheduleFilterViewModel, вынесена вся бизнес-логика из View.

---
+++ Trains/Views/ScheduleFilter/ScheduleFilterViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ScheduleFilterViewModel.swift
+//  Trains
+//
+//  Created by Рустам Ханахмедов on 12.01.2026.
+//
+
+import SwiftUI
+
+struct ScheduleFilterViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScheduleFilterViewModel()
+}
